c826abdaf8eb9348c9e3a14bbc4c17dc389595b1
Your Name <you@example.com>
2017-11-07 22:41:30 +0900

    polls1 생성

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djagpwls10$dagpwls10',
+        'USER': 'djagpwls10',
+        'PASSWORD': 'fprtmf211',
+        'HOST': 'djagpwls10.mysql.pythonanywhere-services.com',
     }
 }
 
