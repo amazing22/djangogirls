5d6bdbe9491fde36723c483e131363503a2f14d5
Your Name <you@example.com>
2017-11-11 16:44:48 +0900

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+   'polls.apps.PollsConfig',
+   'polls1.apps.Polls1Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
